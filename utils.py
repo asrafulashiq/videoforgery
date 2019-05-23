8341139b412e9e597c31bab1fd4b7d7f98454a9d
asrafulashiq <asrafulashiq@gmail.com>
2019-05-23 15:16:06 -0400

    changed transofrm

--- utils.py
+++ utils.py
@@ -8,16 +8,17 @@ import numpy as np
 
 class CustomTransform():
     def __init__(self):
-        self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
+        self.mean = np.array([0.485, 0.456, 0.406], dtype=np.float)
+        self.std = np.array([0.229, 0.224, 0.225], dtype=np.float)
+        # self.normalize = transforms.Normalize(mean, std)
         self.to_tensor = transforms.ToTensor()
 
     def __call__(self, img, mask=None):
         if img.dtype == np.uint8:
             img = img / 255.
 
+        img = (img - self.mean) / self.std
         img = self.to_tensor(img)
-        img = self.normalize(img)
 
         if mask is not None:
             mask = self.to_tensor(mask)
