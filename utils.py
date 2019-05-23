f6299ff3a38173c627bf100e37aab815774a8d0a
asrafulashiq <asrafulashiq@gmail.com>
2019-05-23 15:26:27 -0400

    image resize

--- utils.py
+++ utils.py
@@ -4,10 +4,11 @@ import torch
 from unet_models import UNet11
 from torchvision import transforms
 import numpy as np
-
+import cv2
 
 class CustomTransform():
-    def __init__(self):
+    def __init__(self, size=224):
+        self.size = (size, size)
         self.mean = np.array([0.485, 0.456, 0.406], dtype=np.float)
         self.std = np.array([0.229, 0.224, 0.225], dtype=np.float)
         # self.normalize = transforms.Normalize(mean, std)
@@ -17,10 +18,12 @@ class CustomTransform():
         if img.dtype == np.uint8:
             img = img / 255.
 
+        img = cv2.resize(img, self.size)
         img = (img - self.mean) / self.std
         img = self.to_tensor(img)
 
         if mask is not None:
+            mask = cv2.resize(mask, self.size)
             mask = self.to_tensor(mask)
 
         return img, mask
