7e6a3c29a6f8ce92606fea404ec3495223b954a1
asrafulashiq <ashrafulislam@ash-mac.local>
2019-05-24 18:54:41 -0400

    /

--- dataset.py
+++ dataset.py
@@ -81,6 +81,7 @@ class Dataset_image:
                         counter = 0
             else:  # testing mode
                 pass
+            yield X, Y
 
     # def __len__(self):
     #     return len(self.__im_files_with_gt)
