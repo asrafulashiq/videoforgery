86a630397632fd747279e91ce56478048bbec9a5
asrafulashiq <asrafulashiq@gmail.com>
2019-06-10 14:24:21 -0400

    /

--- run.sh
+++ run.sh
@@ -4,35 +4,39 @@ set -gx CUDA_VISIBLE_DEVICES 2,3
 
 # test on coco pretrained, with and without boundary
 
-python main.py --videoset youtube --model unet  --epoch 20 --lr 1e-4 \
-           --ckpt ckpt/base_unet_coco_bce.pkl >> tmp/coco_pre_utube_unet.txt
+python main.py --videoset youtube --model tunet  --epoch 20 --lr 1e-4 \
+           --ckpt ckpt/base_unet_coco_bce.pkl --validate >> tmp/coco_pre_utube_unet.txt
 
-python main.py --videoset davis --model unet --epoch 20 --lr 1e-4 \
-           --ckpt ckpt/base_unet_coco_bce.pkl >> tmp/coco_pre_davis_unet.txt
+python main.py --videoset davis --model tunet --epoch 20 --lr 1e-4 \
+           --ckpt ckpt/base_unet_coco_bce.pkl --validate >> tmp/coco_pre_davis_unet.txt
 
 
-python main.py --videoset all --model unet --epoch 20 --lr 1e-4 \
-           --ckpt ckpt/base_unet_coco_bce.pkl >> tmp/coco_pre_all_unet.txt
+python main.py --videoset all --model tunet --epoch 20 --lr 1e-4 \
+           --ckpt ckpt/base_unet_coco_bce.pkl --validate >> tmp/coco_pre_all_unet.txt
 
 
 # test with lr
-python main.py --videoset youtube --model unet  --epoch 10 --lr 1e-5 \
-           --ckpt ckpt/base_unet_coco_bce.pkl >> tmp/utube_lr_1.txt
+python main.py --videoset youtube --model tunet  --epoch 10 --lr 1e-5 \
+           --ckpt ckpt/base_unet_coco_bce.pkl --validate >> tmp/utube_lr_1.txt
 
-python main.py --videoset youtube --model unet  --epoch 10 --lr 1e-2 \
-           --ckpt ckpt/base_unet_coco_bce.pkl >> tmp/utube_lr_2.txt
+python main.py --videoset youtube --model tunet  --epoch 10 --lr 1e-2 \
+           --ckpt ckpt/base_unet_coco_bce.pkl --validate >> tmp/utube_lr_2.txt
 
-python main.py --videoset youtube --model unet  --epoch 10 --lr 0.1 \
-           --ckpt ckpt/base_unet_coco_bce.pkl >> tmp/utube_lr_3.txt
+python main.py --videoset youtube --model tunet  --epoch 10 --lr 0.1 \
+           --ckpt ckpt/base_unet_coco_bce.pkl --validate >> tmp/utube_lr_3.txt
 
 
 # test with batch-size
-python main.py --videoset youtube --batch-size 10 --model unet  --epoch 10 --lr 1e-4 \
-           --ckpt ckpt/base_unet_coco_bce.pkl >> tmp/utube_bs_1.txt
+python main.py --videoset youtube --batch-size 10 --model tunet  --epoch 10 --lr 1e-4 \
+           --ckpt ckpt/base_unet_coco_bce.pkl --validate >> tmp/utube_bs_1.txt
 
-python main.py --videoset youtube --batch-size 60 --model unet  --epoch 10 --lr 1e-4 \
-           --ckpt ckpt/base_unet_coco_bce.pkl >> tmp/utube_bs_2.txt
+python main.py --videoset youtube --batch-size 60 --model tunet  --epoch 10 --lr 1e-4 \
+           --ckpt ckpt/base_unet_coco_bce.pkl --validate >> tmp/utube_bs_2.txt
 
+
+# check why davis was inaccurate before (may be overfitting?)
+python main.py --videoset davis --model tunet --epoch 20 --lr 1e-4 \
+    --validate >> tmp/coco_pre_davis_unet.txt
 
 
 ## DAVIS
